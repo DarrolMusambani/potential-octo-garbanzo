0d6f54581d51e260c41d1c3ce42beb7f319711e6
DarrolMusambani <d.mrugalla@googlemail.com>
2017-11-26 13:44:51 +0100

    Update initContainer.sh

--- initContainer.sh
+++ initContainer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#MOUNTPATH, SMB_IP & SHARE_NAME need to be predefined
 CONFIGTARGET="/etc/init.d/fhem"
+DBCONFIG="/opt/fhem/configDB.conf"
 mkdir $MOUNTPATH
 chown fhem:dialout $MOUNTPATH
 mount -t cifs //$SMB_IP/$SHARE_NAME $MOUNTPATH
 cp ${MOUNTPATH}fhem $CONFIGTARGET
+cp ${MOUNTPATH}configDB.conf $DBCONFIG
+chown fhem:dialout $DBCONFIG
 service fhem start
