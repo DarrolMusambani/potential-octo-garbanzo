8f3c90d1fd12e5bff4e89b89b0816656c28dc052
DarrolMusambani <d.mrugalla@googlemail.com>
2017-11-30 20:46:58 +0100

    Update initContainer.sh

--- initContainer.sh
+++ initContainer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#MOUNTPATH, SMB_IP & SHARE_NAME need to be predefined
-CONFIGTARGET="/etc/init.d/fhem"
-DBCONFIG="/opt/fhem/configDB.conf"
-mkdir $MOUNTPATH
-chown fhem:dialout $MOUNTPATH
-mount -t cifs //$SMB_IP/$SHARE_NAME $MOUNTPATH
-cp ${MOUNTPATH}fhem $CONFIGTARGET
-cp ${MOUNTPATH}configDB.conf $DBCONFIG
-chown fhem:dialout $DBCONFIG
+SMB_IP="100.10.1.10"
+SHARE_NAME="public/FhemBackup"
+SMB_PATH="/media/WDMYCLOUD/"
+INIT_FHEM="/etc/init.d/fhem"
+FHEM_PATH="/opt/fhem/"
+mkdir $SMB_PATH
+chown fhem:dialout $SMB_PATH
+mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH
+cp ${SMB_PATH}fhem $INIT_FHEM
+cp ${SMB_PATH}configDB.conf ${FHEM_PATH}configDB.conf
+chown fhem:dialout ${FHEM_PATH}configDB.conf
 service fhem start
