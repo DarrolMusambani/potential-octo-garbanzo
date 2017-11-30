f6662463868ae84e3c993e75df764f0ebf2de8d9
DarrolMusambani <d.mrugalla@googlemail.com>
2017-11-30 21:10:51 +0100

    Update initContainer.sh

--- initContainer.sh
+++ initContainer.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-SMB_IP="100.10.1.10"
-SHARE_NAME="public/FhemBackup"
-SMB_PATH="/media/WDMYCLOUD/"
-INIT_FHEM="/etc/init.d/fhem"
-FHEM_PATH="/opt/fhem/"
 mkdir $SMB_PATH
 chown fhem:dialout $SMB_PATH
-mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH
+mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH -o guest
 cp ${SMB_PATH}fhem $INIT_FHEM
 cp ${SMB_PATH}configDB.conf ${FHEM_PATH}configDB.conf
 chown fhem:dialout ${FHEM_PATH}configDB.conf
