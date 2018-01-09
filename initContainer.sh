3713c07027fbb0dfe2febea6bc56d02fb632c325
DarrolMusambani <d.mrugalla@googlemail.com>
2018-01-09 15:18:52 +0100

    Update initContainer.sh

--- initContainer.sh
+++ initContainer.sh
@@ -4,5 +4,5 @@ chown fhem:dialout $SMB_PATH
 mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH -o guest
 cp ${SMB_PATH}fhem $INIT_FHEM
 cp ${SMB_PATH}configDB.conf ${FHEM_PATH}configDB.conf
-chown fhem:dialout ${FHEM_PATH}configDB.conf
+chown -R fhem:dialout ${FHEM_PATH}
 service fhem start
