03006b5a8af08e255c97f1a7078a46f8eeeb031f
DarrolMusambani <d.mrugalla@googlemail.com>
2018-02-15 15:20:52 +0100

    configDB aktivieren/fhem.cfg deaktivieren
    sed statt vorgefertigter configdatei

--- initContainer.sh
+++ initContainer.sh
@@ -2,7 +2,10 @@
 mkdir $SMB_PATH
 chown fhem:dialout $SMB_PATH
 mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH -o guest
-cp ${SMB_PATH}fhem $INIT_FHEM
-cp ${SMB_PATH}configDB.conf ${FHEM_PATH}configDB.conf
+
+#configDB aktivieren/fhem.cfg deaktivieren
+sed -i -e 's/perl fhem.pl fhem.cfg.*/#perl fhem.pl fhem.cfg/g' /etc/init.d/fhem
+sed -i -e 's/#.*perl fhem.pl configDB.*/perl fhem.pl configDB/g' /etc/init.d/fhem
+
 chown -R fhem:dialout ${FHEM_PATH}
 service fhem start
