7bc767fa3a4f7aa50d1053b373cc3cf1828d649b
DarrolMusambani <d.mrugalla@googlemail.com>
2018-02-15 21:23:40 +0100

    Update create_fhem_db.sql

--- create_fhem_db.sql
+++ create_fhem_db.sql
@@ -6,9 +6,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 
--- Database: fhem
-
--- DROP DATABASE fhem;
+DROP DATABASE fhem;
 
 CREATE DATABASE fhem
     WITH 
@@ -18,43 +16,7 @@ CREATE DATABASE fhem
     LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
-
-GRANT TEMPORARY, CONNECT ON DATABASE fhem TO PUBLIC;
-
+    
 GRANT ALL ON DATABASE fhem TO postgres;
 
 GRANT ALL ON DATABASE fhem TO fhemUser;
-
-CREATE SCHEMA fhem;
-
-ALTER SCHEMA fhem OWNER TO fhemUser;
-
-COMMENT ON SCHEMA fhem IS 'standard fhem schema';
-
-SET search_path = fhem, pg_catalog;
-SET default_tablespace = '';
-SET default_with_oids = false;
-
-CREATE TABLE current (
-    "timestamp" timestamp without time zone,
-    device character varying(64),
-    type character varying(64),
-    event character varying(512),
-    reading character varying(64),
-    value character varying(128),
-    unit character varying(32)
-);
-ALTER TABLE fhem.current OWNER TO fhem;
-
-
-CREATE TABLE history (
-    "timestamp" timestamp without time zone,
-    device character varying(64),
-    type character varying(64),
-    event character varying(512),
-    reading character varying(64),
-    value character varying(128),
-    unit character varying(32)
-);
-ALTER TABLE fhem.history OWNER TO fhem;
-CREATE INDEX "Search_Idx" ON history USING btree (device, reading, "timestamp");
