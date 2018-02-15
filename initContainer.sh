32c7e130b8ab050f9c3c02f2905c85778aaf5337
DarrolMusambani <d.mrugalla@googlemail.com>
2018-02-15 15:48:56 +0100

    Update initContainer.sh

--- initContainer.sh
+++ initContainer.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-mkdir $SMB_PATH
-chown fhem:dialout $SMB_PATH
-mount -t cifs //$SMB_IP/$SHARE_NAME $SMB_PATH -o guest
 
 #configDB aktivieren/fhem.cfg deaktivieren
 sed -i -e 's/perl fhem.pl fhem.cfg.*/#perl fhem.pl fhem.cfg/g' /etc/init.d/fhem
 sed -i -e 's/#.*perl fhem.pl configDB.*/perl fhem.pl configDB/g' /etc/init.d/fhem
+echo -n "" > /opt/fhem/configDB.conf
+echo -n "%dbconfig= (connection => \"Pg:database=$FHEMDB;host=$DBHOST\",user => \"$DBUSER\",password => \"$DBPASSWD\"); " > configDB.conf ^C
 
-chown -R fhem:dialout ${FHEM_PATH}
 service fhem start
